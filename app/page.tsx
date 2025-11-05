5bde0d9867fbf522676cffae67af619f5a4962d7
synbear <ahmedbukhari.uni@gmail.com>
2025-11-05 23:54:32 +0500

    footer and bug fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import Grid from "@/components/ui/Grid";
@@ -18,7 +19,7 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems}/>
         <Hero />
@@ -27,6 +28,7 @@ export default function Home() {
         <Clients />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
---
+++ components/Footer.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
+import { socialMedia } from '@/data';
+
+const Footer = () => {
+const year = new Date().getFullYear();
+
+  return (
+    <footer className='w-full pb-10 mb-[10px] md:mb-5' id='contact'>
+    <div className='flex flex-col items-center'>
+      <h1 className='heading lg:max-w-[45vw]'>
+        Ready to take <span className='text-purple-300'>your</span>digital presence to the next level?
+      </h1>
+      <p className='text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let&apos;s discuss how I can help you achieve your goals.</p>
+      <a href='mailto:ahmedbukhari.uni@gmail.com'>
+        <MagicButton 
+        title="Let's get in touch"
+        icon={<FaLocationArrow />}
+        position='right'
+        />
+      </a>
+    </div>
+    <div>
+        <p className=' pt-5 md:text-base text-sm md:font-normal font-light'>Copyright © {year} synbear</p>
+        <div className='pt-5 flex items-center md:gap-3 gap-6'>
+        {socialMedia.map((profile =>
+            <div key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
+                <img src={profile.img} alt={String(profile.id)} width={20} height={20} />
+            </div>
+        ))}
+        </div>
+    </div>
+    </footer>
+  )
+}
+
+export default Footer
