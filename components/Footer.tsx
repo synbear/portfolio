38dcd1674007c67e6000c7f2a010d27298aa19b4
synbear <ahmedbukhari.uni@gmail.com>
2025-11-06 00:37:15 +0500

    minor spacing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,7 +10,7 @@ const year = new Date().getFullYear();
     <footer className='w-full pb-10 mb-[10px] md:mb-5' id='contact'>
     <div className='flex flex-col items-center'>
       <h1 className='heading lg:max-w-[45vw]'>
-        Ready to take <span className='text-purple-300'>your</span>digital presence to the next level?
+        Ready to take <span className='text-purple-300'>your</span> digital presence to the next level?
       </h1>
       <p className='text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let&apos;s discuss how I can help you achieve your goals.</p>
       <a href='mailto:ahmedbukhari.uni@gmail.com'>
