c703b2f8669eb81cadade626c7e7594b96c6e141
synbear <ahmedbukhari.uni@gmail.com>
2025-11-05 22:00:52 +0500

    Approach Section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -33,9 +33,9 @@ const Hero = () => {
             </h2>
             <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl" words="Transforming Concepts into Seamless User Experiences"/>
             <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-              Hi, I&apos;m Ahmed based in Pakistan.
+              Hi, I&apos;m Ahmed Bukhari, a full-stack developer based in Pakistan.
             </p>
-            <a href="#about">
+            <a href="#projects">
               <MagicButton 
               title="Show my work" 
               icon={<FaLocationArrow />}
